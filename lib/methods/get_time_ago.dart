98dbb1b47f9c7e8bf8ac6cdfffaf345308977d60
tuanloc-vocong <vocong.tuanloc@gmail.com>
2024-03-19 23:07:54 +0700

    init

---
+++ lib/methods/get_time_ago.dart
@@ -0,0 +1,25 @@
+extension StringExtension on String {
+  String displayTimeFromTimestamp(String timestamp) {
+    final year = int.parse(timestamp.substring(0, 4));
+    final month = int.parse(timestamp.substring(5, 7));
+    final day = int.parse(timestamp.substring(8, 10));
+    final hour = int.parse(timestamp.substring(11, 13));
+    final minute = int.parse(timestamp.substring(14, 16));
+
+    final DateTime videoDate = DateTime(year, month, day, hour, minute);
+    final int diffInHours = DateTime.now().difference(videoDate).inHours;
+
+    String timeAgo = '';
+    String timeUnit = '';
+    int timeValue = 0;
+
+    if (diffInHours < 1) {
+      final diffInMinutes = DateTime.now().difference(videoDate).inMinutes;
+      timeValue = diffInMinutes;
+      timeUnit = 'minute';
+    } else if (diffInHours < 24) {
+      timeValue = diffInHours;
+      timeUnit = 'hour';
+    }
+  }
+}
